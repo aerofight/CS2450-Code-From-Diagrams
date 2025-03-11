c14618d49d28e1b1df895e6a101fbe7a3e33d234
aaronheiner1 <aaronheiner1@mail.weber.edu>
2025-03-10 20:20:57 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -3,4 +3,16 @@ public class Order {
     private int estimatedDeliveryTime = 0;
     private int TrackingNumber = 0;
     private String orderId = "";
+
+    public int getPrice() { return price; };
+    public int setPrice(){} ;
+
+    public int getEstimatedDeliveryTime() { return estimatedDeliveryTime; };
+    public int setEstimatedDeliveryTime() {};
+
+    public int getTrackingNumber() { return TrackingNumber; };
+    public int setTrackingNumber() {};
+
+    public int getOrderId() {return TrackingNumber; };
+    public int setPrice(){} ;
 }
