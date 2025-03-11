de4b82965c246de25aee001df73bc3528c7bb29c
LukePeterson1 <lukepeterson1@mail.weber.edu>
2025-03-10 20:03:29 -0600

    User class

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {}
+
+    public String getUsername() {return username;}
+    public String getPassword() {return password;}
+    public void setUsername(String username) {}
+    public void setPassword(String password) {}
+
+
+}
