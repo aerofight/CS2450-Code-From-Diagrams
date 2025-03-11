7fab79b5c5b6a7d6e549cd648066d947ae48b1ac
aerofight <105453005+aerofight@users.noreply.github.com>
2025-03-10 20:41:56 -0600

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,7 +2,15 @@ public class Admin extends Employee {
     private String clearanceLevel;
 
     public Admin(String username, String password, String clearanceLevel, String name, int salary) {
-        super(username, password, name, salary);
+        super(name, salary, username, password);
         this.clearanceLevel = clearanceLevel;
+    }
+
+    public void setClearanceLevel(String clearanceLevel) {
+        this.clearanceLevel = clearanceLevel;
+    }
+
+    public String getClearanceLevel() {
+        return clearanceLevel;
     }
 }
