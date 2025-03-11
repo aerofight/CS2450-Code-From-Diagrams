03160657e3e654703ba4e25096958614d2865e05
aerofight <105453005+aerofight@users.noreply.github.com>
2025-03-10 20:51:12 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -2,17 +2,40 @@ public class Order {
     private int price = 0;
     private int estimatedDeliveryTime = 0;
     private int TrackingNumber = 0;
-    private String orderId = "";
+    private int orderId;
 
-    public int getPrice() { return price; };
-    public int setPrice(int price){this.price = price;} ;
+    public Order(int price, int estimatedDeliveryTime, int TrackingNumber, int orderId) {
+        this.price = price;
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+        this.TrackingNumber = TrackingNumber;
+        this.orderId = orderId;
+    }
 
-    public int getEstimatedDeliveryTime() { return estimatedDeliveryTime; };
-    public int setEstimatedDeliveryTime(int estimatedDeliveryTime) {this.estimatedDeliveryTime = estimatedDeliveryTime;};
+    public int getPrice() {
+        return price;
+    };
+    public void setPrice(int price){
+        this.price = price;
+    } ;
 
-    public int getTrackingNumber() { return TrackingNumber; };
-    public int setTrackingNumber(int TrackingNumber) {this.TrackingNumber = TrackingNumber;};
+    public int getEstimatedDeliveryTime() {
+        return estimatedDeliveryTime;
+    };
+    public void setEstimatedDeliveryTime(int estimatedDeliveryTime) {
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+    };
 
-    public int getOrderId() {return TrackingNumber; };
-    public int setOrderId(int orderId ){this.orderId = orderId} ;
+    public int getTrackingNumber() {
+        return TrackingNumber;
+    };
+    public void setTrackingNumber(int TrackingNumber) {
+        this.TrackingNumber = TrackingNumber;
+    };
+
+    public int getOrderId() {
+        return TrackingNumber;
+    };
+    public void setOrderId(int orderId ){
+        this.orderId = orderId;
+    };
 }
